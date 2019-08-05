35970bb6a823d4950eda41cbac3b2dcf042cd1b1
unknown <ShaoWei>
2019-08-05 12:02:29 +0800

    added on form submit method

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+    onSearchSubmit = term => {
+        console.log(term);
+    };
+
     render() {
         return (
             <div className="ui container" style={{ marginTop: "20px" }}>
-                <SearchBar />
+                <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
         );
     }
